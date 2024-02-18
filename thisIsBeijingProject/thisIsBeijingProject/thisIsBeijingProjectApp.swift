6690e8b13e3c1508fc80ecdb77d95afc9c1d8564
changyuan <changyuan>
2024-02-19 00:10:27 +0800

    项目初始化

---
+++ thisIsBeijingProject/thisIsBeijingProject/thisIsBeijingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  thisIsBeijingProjectApp.swift
+//  thisIsBeijingProject
+//
+//  Created by 舞极灵瞳 on 2024/2/19.
+//
+
+import SwiftUI
+
+@main
+struct thisIsBeijingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
